33c018aee2bab84948782b5c24a290317745c20e
ramprasathmk <mkramprasath12@gmail.com>
2024-12-22 01:24:52 +0545

    Refactored INSERT statement

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- Dumping Data for table: Users
 INSERT INTO Users (username, name, password) VALUES ('davegrey', 'DaveGrey', '123456');
 INSERT INTO Users (username, name, password) VALUES ('johndoe', 'JohnDoe', '123456');
-INSERT INTO Users (username, name, password) VALUES ('rahul12', 'Rahul', '123456');
+INSERT INTO Users (username, name, password) VALUES ('luffy56', 'Luffy', '123456');
 INSERT INTO Users (username, name, password) VALUES ('roger11', 'Roger', '123456');
 
 -- Dumping Data for table: Quotes

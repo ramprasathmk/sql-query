fb4b63ef3a4e70b2934dee928b17eee661be40a8
ramprasathmk <mkramprasath12@gmail.com>
2024-12-22 00:58:11 +0545

    insert values into Users table

--- data.sql
+++ data.sql
@@ -1,2 +1,9 @@
 -- insert the data here!
 
+-- Dumping Data for table: Users
+INSERT INTO Users(user_name, password) VALUES ('DaveGrey', 'dave123');
+INSERT INTO Users(user_name, password) VALUES ('JohnDoe', 'john123');
+INSERT INTO Users(user_name, password) VALUES ('Rahul', 'rahul123');
+INSERT INTO Users(user_name, password) VALUES ('Roger', 'roger111');
+
+-- Dumping Data for table: Quotes
